adb89f8f8484f0010ab91d576d8ff5ebca0f5ec5
yourboyfred <fredrickkimathi26@gmail.com>
2024-05-03 15:21:57 +0300

    minor changes

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function index(){
+        return view('welcome');
+    }
+    public function store(){
+
+        $attributes = request()->validate([
+            'title' => 'required',
+            'description' => 'nullable'
+
+
+        ]);
+
+        Todo::create($attributes);
+
+        return redirect('/')->with('success','');
+
+
+    }
+}
